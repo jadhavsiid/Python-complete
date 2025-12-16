6066845426f714c4c7b02ce06115cb6472d96532
jadhavsiid <siidjadhav170801@gmail.com>
2025-12-16 21:12:47 +0530

    Day-1 Progress

---
+++ 1_Intro-to-python/hello_python.py
@@ -0,0 +1,11 @@
+print("Hello Python")
+
+def hello(a):
+    print(a)
+
+hello(4)
+hello('We are Learning Python')
+
+ineng = "Hello"
+inspa = "Hola"
+infrench = "Bonjour"
---
+++ 1_Intro-to-python/second_file.py
@@ -0,0 +1,2 @@
+from hello_python import hello
+hello("This is a hello method from second file")
